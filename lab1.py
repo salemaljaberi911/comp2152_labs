51e6d0232a97aefdf079fd5be3913545bba7f097
Salem Aljabri <salem.aljbri11@gmail.com>
2026-01-22 09:29:09 +0400

    lab1

---
+++ lab1.py
@@ -0,0 +1,19 @@
+# Sample Coding Questions 01 Week 01
+# First Name: Salem
+# Last Name: Aljabri
+my_array = [1, 4, 7, 9]
+
+a = 1
+b = 2
+c = 3
+d = 4
+
+e = (a * c) - (b / d)
+e = (a - ((b ** c) // d)) + (a % c)
+
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+userAge = int(input("Enter your age: "))
+userAge += 22
+print("Now showing the shop items filtered by age:", userAge)
